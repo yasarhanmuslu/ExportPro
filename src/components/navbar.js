fb7c1192c0654da62daf254493a9db63b5289db2
yasarhanmuslu <yasarhan.m@gmail.com>
2026-05-24 23:20:02 +0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import { supabase } from '../utils/supabaseClient.js';
 
-const APP_VERSION = 'V: 1.0.19';
+const APP_VERSION = 'V: 1.0.20';
 
 export async function renderNavbar(activeTab) {
     const { data: { session } } = await supabase.auth.getSession();
