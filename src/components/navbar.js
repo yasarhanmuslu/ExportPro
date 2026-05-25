33cf365d494ff9bb9142e8748e56bab5fdac66e6
berkantozgun <berkantozgun@gmail.com>
2026-05-26 00:08:06 +0300

    1.0.33

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import { supabase } from '../utils/supabaseClient.js';
 
-const APP_VERSION = 'V: 1.0.32';
+const APP_VERSION = 'V: 1.0.33';
 
 export async function renderNavbar(activeTab) {
     const { data: { session } } = await supabase.auth.getSession();
